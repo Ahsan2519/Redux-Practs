70e6445f4390c77ea51c264a3e4fe5674051ee4e
Ahsan2519 <ansariahsan67@gmail.com>
2023-09-08 18:49:43 +0530

    Added: cake with icecrea redux logic added

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ const createStore = redux.createStore;
 const bindActionCreators = redux.bindActionCreators;
 const CAKE_ORDER = "CAKE_ORDER";
 const RESTOCK_CAKE = "RESTOCK_CAKE";
+const ICECREAM_ORDER = "ICECREAM_ORDER";
+const RESTOCK_ICECREAM = "RESTOCK_ICECREAM";
 
 const orderCake = () => {
   return {
     type: CAKE_ORDER,
+    payload: 1,
+  };
+};
+const orderIcecream = () => {
+  return {
+    type: ICECREAM_ORDER,
     payload: 1,
   };
 };
@@ -16,8 +24,15 @@ const resStockCake = (qty) => {
     payload: qty,
   };
 };
+const resStockIcecream = (qty) => {
+  return {
+    type: RESTOCK_ICECREAM,
+    payload: qty,
+  };
+};
 const initialState = {
   nunmOfCake: 10,
+  numberOfIcecream: 25,
 };
 
 const reducer = (state = initialState, action) => {
@@ -32,6 +47,16 @@ const reducer = (state = initialState, action) => {
         ...state,
         nunmOfCake: state.nunmOfCake + action.payload,
       };
+    case ICECREAM_ORDER:
+      return {
+        ...state,
+        numberOfIcecream: state.numberOfIcecream - 1,
+      };
+    case RESTOCK_ICECREAM:
+      return {
+        ...state,
+        numberOfIcecream: state.numberOfIcecream + action.payload,
+      };
     default:
       return state;
   }
@@ -42,7 +67,10 @@ console.log("initialSate:", store.getState());
 const unsubscribe = store.subscribe(() =>
   console.log("Updated Sate:", store.getState())
 );
-const actions = bindActionCreators({ orderCake, resStockCake }, store.dispatch);
+const actions = bindActionCreators(
+  { orderCake, resStockCake, orderIcecream, resStockIcecream },
+  store.dispatch
+);
 // store.dispatch(orderCake());
 // store.dispatch(orderCake());
 // store.dispatch(orderCake());
@@ -50,4 +78,7 @@ actions.orderCake();
 actions.orderCake();
 actions.orderCake();
 actions.resStockCake(3);
+actions.orderIcecream();
+actions.orderIcecream();
+actions.resStockIcecream(2);
 unsubscribe();
