5a17b77fe5b45e239b68dfe1ca5bb962e2cc8ef7
Ahsan2519 <ansariahsan67@gmail.com>
2023-09-10 20:26:37 +0530

    Added: immer added to update state

--- post.js
+++ post.js
@@ -5,6 +5,7 @@ const applyMiddleware = redux.applyMiddleware;
 const reduxLogger = require("redux-logger");
 const logger = reduxLogger.createLogger();
 const reduxThunk = require("redux-thunk").default;
+const produce = require("immer").produce;
 
 const USERS_PENDING = "USERS_PENDING";
 const USERS_FULFIL = "USERS_FULFIL";
@@ -44,15 +45,28 @@ const initialSate = {
 const reducer = (state = initialSate, action) => {
   switch (action.type) {
     case USERS_PENDING:
-      return { ...state, isPending: true };
+        // with immer
+      return produce(state, (draft) => {
+        draft.isPending = true;
+      });
+    //   without immer
+    //   return { ...state, isPending: true };
     case USERS_FULFIL:
-      return {
-        ...state,
-        isPending: false,
-        data: action.payload.value,
-      };
+      return produce(state, (draft) => {
+        draft.isPending = false;
+        draft.data = action.payload.value;
+      });
+    //   return {
+    //     ...state,
+    //     isPending: false,
+    //     data: action.payload.value,
+    //   };
     case USERS_REJECTED:
-      return { ...state, isPending: false, error: action.payload.error };
+      return produce(state, (draft) => {
+        draft.isPending = false;
+        draft.error = action.payload.error;
+      });
+    //   return { ...state, isPending: false, error: action.payload.error };
     default:
       return state;
   }
